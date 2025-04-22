3dcadf692cfe6ea6648ab995af6b290b4e17d0b3
Irakli <i.waqadze1999@gmail.com>
2025-04-22 17:21:57 +0400

    fix home button routing  problem part 4

--- js/utils/githubUrl.js
+++ js/utils/githubUrl.js
@@ -1,4 +1,4 @@
-export const githubUrl = window.location.pathname.split("/")[1] || "";
+export const githubUrl = window.location.pathname || "";
 
 console.log(window.location);
 console.log(window.location.pathname);
